d35ccbfdafcf1db1efd0abc67e12ba4386625fe5
Adam Imbert <apimbert@ncsu.edu>
2021-10-07 23:36:15 -0400

    Gitignore (again), Grid/Actor classes, tilemap started

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor
+{
+    private Cardinal facing;
+    private Vector2Int gridPosition;
+    public enum Cardinal
+    { 
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
+
+    public Actor(Vector2Int gridPosition, Cardinal facing)
+    {
+        this.gridPosition = gridPosition;
+        this.facing = facing;
+    }
+}
---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    private Grid grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new Grid(8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
