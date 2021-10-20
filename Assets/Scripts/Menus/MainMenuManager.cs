b470a61d7f02f1d0f81aeb1ef93f4e8650abbe55
Prismly <prismly55@gmail.com>
2021-10-20 06:24:26 -0400

    Started messing with level selection, it *almost* works but I'll leave it here for the night

--- Assets/Scripts/Menus/MainMenuManager.cs
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -10,9 +10,12 @@ public class MainMenuManager : MonoBehaviour
     private string gameplaySceneName = "Game Scene";
     [SerializeField] private TextAsset[] levels;
     [SerializeField] private Text titleText;
-    [SerializeField] private Button newGameButton;
-    [SerializeField] private Button continueButton;
-    [SerializeField] private Button levelSelectButton;
+    [SerializeField] private Text maxULText;
+    [SerializeField] private GameObject newGameButton;
+    [SerializeField] private GameObject continueFromButton;
+    [SerializeField] private GameObject advanceLeftButton;
+    [SerializeField] private GameObject advanceRightButton;
+    [SerializeField] private GameObject resetButton;
 
     [SerializeField] private Tilemap bg1;
     [SerializeField] private Tilemap bg2;
@@ -20,12 +23,42 @@ public class MainMenuManager : MonoBehaviour
 
     public void Start()
     {
-        continueButton.GetComponentInChildren<Text>().text = "Continue from: " + StaticData.GetCurrentLevelUnlocked();
+        StaticData.InitializeLevelArray(levels);
+        continueFromButton.GetComponentInChildren<Text>().text = "Continue From: " + StaticData.GetLevelSelected();
+        maxULText.text = "Next Level: " + StaticData.GetCurrentLevelUnlocked();
+    }
+
+    public void ResetProgress()
+    {
+        StaticData.SetLevelSelected(1);
+        StaticData.SetCurrentLevelUnlocked(1);
+        continueFromButton.GetComponentInChildren<Text>().text = "Continue From: " + StaticData.GetCurrentLevelUnlocked();
+    }
+
+    public void IncrementSelectedLevel(int diff)
+    {
+        int result = StaticData.GetLevelSelected() + diff;
+        if(result <= 1)
+        {
+            result = 1;
+        }
+        else if(result >= StaticData.GetCurrentLevelUnlocked())
+        {
+            result = StaticData.GetCurrentLevelUnlocked();
+        }
+
+        StaticData.SetLevelSelected(result);
+        continueFromButton.GetComponentInChildren<Text>().text = "Continue From: " + StaticData.GetLevelSelected();
     }
 
     public void LoadLevel(int levelNum)
     {
-        GameGrid.SetLayout(levels[levelNum]);
+        GameGrid.SetLayout(StaticData.LayoutFromLevelNumber(levelNum));
         SceneManager.LoadScene(gameplaySceneName);
+    }
+
+    public void LoadSelectedLevel()
+    {
+        LoadLevel(StaticData.GetLevelSelected());
     }
 }
